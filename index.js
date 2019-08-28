18e4c1b9448c882015890486fe54819021475a53
Archomeda <archomeda@hotmail.com>
2019-08-28 21:54:33 +0200

    Fix a bunch of linting errors

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ if (settings.apiKey) {
 
 const cacheOptions = {
     storageKey: 'gw2-console'
-}
+};
 
 api.cacheStorage(cacheBrowserStorage(cacheOptions));
 
 async function renderIcons() {
     const icons = Array.from(document.querySelectorAll('.item.item-unrendered'));
-    const item_ids = icons.map((element) => (element.attributes['data-item-id'].value)).filter((elem) => (elem && elem != ''));
-    if (item_ids.length == 0) return;
-    const items = await api.items().many(item_ids);
-    items.forEach(({id, icon, rarity, name}) => {
-        const item_containers = document.querySelectorAll(`.item-unrendered.item-${id}`);
-        const item_images = document.querySelectorAll(`.item-unrendered.item-${id} .item-icon`);
-        const item_names = document.querySelectorAll(`.item-unrendered.item-${id} .item-name`);
-        item_containers.forEach((container) => (container.classList.add(rarity.toLowerCase())));
-        item_images.forEach((image) => (image.src = icon));
-        item_names.forEach((item_name) => (item_name.innerHTML = name));
+    const itemIds = icons.map((element) => (element.attributes['data-item-id'].value)).filter((elem) => (elem && elem != ''));
+    if (itemIds.length == 0) return;
+    const items = await api.items().many(itemIds);
+    items.forEach(({ id, icon, rarity, name }) => {
+        const itemContainers = document.querySelectorAll(`.item-unrendered.item-${id}`);
+        const itemImages = document.querySelectorAll(`.item-unrendered.item-${id} .item-icon`);
+        const itemNames = document.querySelectorAll(`.item-unrendered.item-${id} .item-name`);
+        itemContainers.forEach((container) => (container.classList.add(rarity.toLowerCase())));
+        itemImages.forEach((image) => (image.src = icon));
+        itemNames.forEach((itemName) => (itemName.innerHTML = name));
     });
     icons.forEach((icon) => (icon.classList.remove('item-unrendered')));
 }
@@ -62,7 +62,7 @@ const terminal = new Terminal('terminal', {
             return `<span style='color:red;'>${err}</span>`;
         }
     },
-    after_execute: async function (cmd, args) {
+    afterExecute: async function (cmd, args) {
         await renderIcons();
     },
     tabComplete: async function(prefix, index) {
@@ -75,9 +75,9 @@ const terminal = new Terminal('terminal', {
     }
 });
 
-terminal.renderItem = function(item_id, count = -1, name = '') {
-    const display_count = count != -1 ? `${count} ` : '';
-    return `<span class='item item-${item_id} item-unrendered' data-item-id='${item_id}'><img class='item-icon'/><span class='item-label'><span class='item-count'>${display_count}</span><span class='item-name'>${name}</span></span></span>`;
+terminal.renderItem = function(itemId, count = -1, name = '') {
+    const displayCount = count != -1 ? `${count} ` : '';
+    return `<span class='item item-${itemId} item-unrendered' data-item-id='${itemId}'><img class='item-icon'/><span class='item-label'><span class='item-count'>${displayCount}</span><span class='item-name'>${name}</span></span></span>`;
 };
 
 async function wrapApi(promise) {
